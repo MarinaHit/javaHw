b3c8993f62a7cab535886cd04f9b0eccb2b3b6fb
MarinaHit <62765149+MarinaHit@users.noreply.github.com>
2020-04-22 18:23:57 +0300

    HW01 gradle

---
+++ hw01-gradle/src/main/java/ru/mrsk/HellowOtus.java
@@ -0,0 +1,13 @@
+package ru.mrsk;
+
+
+
+public class HellowOtus {
+
+    public static void main(String[] args) {
+       String testGuava = "Hellow OTUS!!!";
+       System.out.println(testGuava.split(" ").length);
+    }
+
+
+}
